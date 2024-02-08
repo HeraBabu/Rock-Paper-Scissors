8438ec53034e43a2625964c5af789b753377be7b
Hera Babu <52198080+HeraBabu@users.noreply.github.com>
2024-02-08 15:19:23 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,60 +1,65 @@
 /* --- VARIABLES --- */
-  const choices = ['✊', '🤚', '✌️'];
-  const player1 = document.getElementById('player-1');
+const choices = ['✊', '🤚', '✌️'];
+const player1 = document.getElementById('player-1');
 
-  const player2 = document.getElementById('player-2');
+const player2 = document.getElementById('player-2');
 
-  const resultArea = document.getElementById('result-area');
+const resultArea = document.getElementById('result-area');
 
-  const playBtn = document.getElementById('play-btn');
-
-  /* This will help check your results 
-  console.log(player2, resultArea, playBtn);*/
+const playBtn = document.getElementById('play-btn');
 
 /* ----------------------------------------------- */
 
 /* --- FUNCTIONS --- */
 
-  const generateChoice = () => {
-    let r = Math.floor(Math.random() * 3);
-    return choices[r]
-  };
+const generateChoice = () => {
+  let r = Math.floor(Math.random() * 3);
+  return choices[r]
+};
 
-  const insertHTML = (choice1, choice2, result) => {
-    player1.innerHTML = choice1;
-    player2.innerHTML = choice2;
-    resultArea.innerHTML = result;
-  };
+const insertHTML = (choice1, choice2, result) => {
+  player1.innerHTML = choice1;
+  player2.innerHTML = choice2;
+  resultArea.innerHTML = result;
+};
 
-  const decideWinner = (a, b) => {
-    if ((a === '✊' && b === '✊') || (a === '🤚' && b === '🤚') || (a === '✌️' && b === '✌️')) {
+const decideWinner = (a, b) => {
+  if ((a === '✊' && b === '✊') || (a === '🤚' && b === '🤚') || (a === '✌️' && b === '✌️')) {
 
-       return "Draw";
+     return "Draw";
 
-    } else if ((a === '✊' && b === '✌️') || (a === '🤚' && b === '✊') || (a === '✌️' && b === '🤚')) {
+  } else if ((a === '✊' && b === '✌️') || (a === '🤚' && b === '✊') || (a === '✌️' && b === '🤚')) {
 
-      return "Player 1 wins";
+    return "Bot wins";
 
-    } else {
+  } else {
 
-      return "Player 2 wins";
+    return "You win";
 
-    }
-  };
+  }
+};
 
-  const play = () => {
-    let choice1 = generateChoice();
-    let choice2 = generateChoice();
-    let result = decideWinner(choice1, choice2);
-
-    insertHTML(choice1, choice2, result);
-  };
+const play = (userch) => {
+  let choice1 = generateChoice();
+  let result = decideWinner(choice1, userch);
+  insertHTML(choice1, userch, result);
+};
 
 /* ----------------------------------------------- */
 
 /* --- EVENT LISTENERS --- */
+var buttons = document.getElementsByClassName('hand-btn');
+  console.log(buttons);
+  var buttonsLength = buttons.length;
+  for (var i = 0; i < buttonsLength; i++){
+    buttons[i].addEventListener('click', clickResponse, false);
+  };
+  function clickResponse(){
+    // do something based on button selection here...
+    var ch2 = (this.id);
+    let userch = document.getElementById(ch2).innerText;
+    play(userch);
+}
 
-  /* 2. */
-  playBtn.addEventListener("click" , play);
 
-/* ------------------------------- */
+
